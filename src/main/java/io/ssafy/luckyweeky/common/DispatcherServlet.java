96d7007d1b8a7137944e9e12953d160b15588827
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-11-24 00:03:23 +0900

    infra:cicd-32

--- src/main/java/io/ssafy/luckyweeky/common/DispatcherServlet.java
+++ src/main/java/io/ssafy/luckyweeky/common/DispatcherServlet.java
@@ -46,6 +46,11 @@ public class DispatcherServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         String uri = request.getRequestURI();
+        if(uri.equals("/")){
+            // health check, HTTP OK return
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
 
         String path = RequestUrlPath.getURI(request.getRequestURI())[0];
         if(path.equals("qweSJqwo")){
